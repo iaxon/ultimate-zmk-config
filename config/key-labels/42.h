e7b61f55ac958cee7a57f3b9b531a3d98d82e5a4
iaxon <jakobgaf@web.de>
2026-05-21 22:45:37 +0200

    feat: implement unified 42-key Corne layout with cross-OS macros

---
+++ config/key-labels/42.h
@@ -0,0 +1,51 @@
+/*
+ * Key labels for 42-key Corne (6x3+3)
+ * Maps urob's 36-key labels to 42-key positions
+ */
+
+#define LT4  1
+#define LT3  2
+#define LT2  3
+#define LT1  4
+#define LT0  5
+#define RT0  6
+#define RT1  7
+#define RT2  8
+#define RT3  9
+#define RT4 10
+
+#define LM4 13
+#define LM3 14
+#define LM2 15
+#define LM1 16
+#define LM0 17
+#define RM0 18
+#define RM1 19
+#define RM2 20
+#define RM3 21
+#define RM4 22
+
+#define LB4 25
+#define LB3 26
+#define LB2 27
+#define LB1 28
+#define LB0 29
+#define RB0 30
+#define RB1 31
+#define RB2 32
+#define RB3 33
+#define RB4 34
+
+#define LH2 36
+#define LH1 37
+#define LH0 38
+#define RH0 39
+#define RH1 40
+#define RH2 41
+
+/*
+ * Groups for HRM and other behaviors
+ */
+#define KEYS_L 1 2 3 4 5 13 14 15 16 17 25 26 27 28 29
+#define KEYS_R 6 7 8 9 10 18 19 20 21 22 30 31 32 33 34
+#define THUMBS 36 37 38 39 40 41
